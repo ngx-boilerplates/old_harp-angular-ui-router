adb99d9c787e512bb33a8a5e114f755d7ab36832
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2014-12-17 15:10:54 +0100

    feat(homepage): add HomepageCtrl controller

---
+++ ngx/public/components/homepage/_build/homepage.controller.js
@@ -0,0 +1,22 @@
+(function () {
+
+  /**
+   * Homepage controller
+   *
+   * @param $scope
+   * @constructor
+   */
+  function HomepageCtrl($scope){
+
+    // ADD CUSTOM LOGIC HERE
+
+  }
+
+  // Inject dependencies
+  HomepageCtrl.$inject = ['$scope'];
+
+  // Assign
+  angular.module('app')
+    .controller('HomepageCtrl', HomepageCtrl);
+
+})();
